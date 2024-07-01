f3a07647a6bcd3e9c40ae22b7a77c063af6bc8e9
Tencarrothill <ultrapandemonium@gmail.com>
2024-07-01 22:48:49 +0900

    0701 alora

--- pratique_04/js/main.js
+++ pratique_04/js/main.js
@@ -2,25 +2,14 @@ $(document).ready(function(){
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
-            delay: 25000,
+            delay: 2500,
             disableOnInteraction: true,
         },
     
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-    
-        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.swiper-pagination', /* 해당 요소의 class명 */
-            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-            renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-                return '<span class="' + className + '">' + (index + 1) + "</span>";
-            },
-        },
-        
-    
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */    
         navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.btn_next',  /* 다음 버튼의 클래스명 */
-            prevEl: '.btn_prev',  
+            nextEl: '.visual .btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .btn_prev',  
         },
     
     });//visual_swiper
