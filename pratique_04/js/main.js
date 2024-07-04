eecde21618b1273009f710a3117cddfd8cda7c91
Tencarrothill <ultrapandemonium@gmail.com>
2024-07-04 23:56:13 +0900

    0704 alora

--- pratique_04/js/main.js
+++ pratique_04/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
-            delay: 2500,
+            delay: 3500,
             disableOnInteraction: true,
         },
     
@@ -13,4 +13,17 @@ $(document).ready(function(){
         },
     
     });//visual_swiper
+
+    const signiture_swiper = new Swiper('.signiture .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+        spaceBetween: 16, /* li와 li사이 - 제일 작은 여백 */
+        breakpoints: {
+            640: {  /* 640px 이상이 되면 적용 */
+            slidesPerView: "auto",
+                spaceBetween: 24,
+            },
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    });//book_swiper
+
 })//document.ready
